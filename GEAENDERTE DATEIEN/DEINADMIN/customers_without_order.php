d20476b1069939787111f4d9d6fcf39a472220ff
webchills <office@webchills.com>
2019-07-20 17:36:40 +0200

    1.5.6c Fix in customers_without_order.php

--- GEAENDERTE DATEIEN/DEINADMIN/customers_without_order.php
+++ GEAENDERTE DATEIEN/DEINADMIN/customers_without_order.php
@@ -3,8 +3,8 @@
  * @package admin
  * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: customers_without_order.php for COWOA 2019-07-04 07:35:51Z webchills $
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+ * @version $Id: customers_without_order.php for COWOA 2019-07-20 17:35:51Z webchills $
  */
 require('includes/application_top.php');
 
@@ -1353,7 +1353,7 @@ if (zen_not_null($action)) {
                       <?php echo zen_image(DIR_WS_IMAGES . 'icon_red_off.gif', IMAGE_ICON_STATUS_OFF); ?>
                       <?php
                     } else {
-                      echo zen_draw_form('setstatus', FILENAME_CUSTOMERS_WITHOUT_ORDER, 'action=status&cID=' . $customer['customers_id'] . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '') . (isset($_GET['search']) ? '&search=' . $_GET['search'] : ''));
+                      echo zen_draw_form('setstatus_' . (int)$customer['customers_id'], FILENAME_CUSTOMERS_WITHOUT_ORDER, 'action=status&cID=' . $customer['customers_id'] . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '') . (isset($_GET['search']) ? '&search=' . $_GET['search'] : ''));
                       ?>
                       <?php if ($customer['customers_authorization'] == 0) { ?>
                       <input type="image" src="<?php echo DIR_WS_IMAGES ?>icon_green_on.gif" title="<?php echo IMAGE_ICON_STATUS_ON; ?>" />
